8f60b9557ba1f64985352dc2197f4eba3f81d494
iRsonHar <iRsonHar@HARRISON-PC>
2016-10-14 04:24:09 -0400

    I think I fixed github shit

---
+++ main/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "targetver.h"
+#include <Windows.h>
+#include <cstdlib>
+#include <conio.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Console.h"
+
+using namespace std;
+using namespace System;
